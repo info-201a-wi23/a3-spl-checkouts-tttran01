6ba819caee30156656ffeb92d9b691800018704c
tttran01 <tran10@uw.edu>
2023-02-23 13:40:38 -0800

    fix chart 1 df

--- Chart-1.R
+++ Chart-1.R
@@ -1,10 +1,29 @@
+library(dplyr)
 
 series1_df <- spl_df %>% 
-  drop_na(Checkouts) %>% 
-  group_by(Title) %>% 
-  filter(str_detect(Title, "Throne of Glass"))
+  select(CheckoutYear, CheckoutMonth, Checkouts, Title, Creator, date) %>% 
+  filter(str_detect(Title, "Throne of Glass")) 
 
-ggplot(data = series1_df) +
-  geom_line(aes(x = x_values, 
-                y = y_values,
+series1_df$Title[str_detect(series1_df$Title, "Crown of Midnight")] <- "Crown of Midnight"
+series1_df$Title[str_detect(series1_df$Title, "Heir of Fire")] <- "Heir of Fire"
+series1_df$Title[str_detect(series1_df$Title, "Queen of Shadows")] <- "Queen of Shadows"
+series1_df$Title[str_detect(series1_df$Title, "Empire of Storms")] <- "Empire of Storms"
+series1_df$Title[str_detect(series1_df$Title, "Tower of Dawn")] <- "Tower of Dawn"
+series1_df$Title[str_detect(series1_df$Title, "Kingdom of Ash")] <- "Kingdom of Ash"
+series1_df$Title[str_starts(series1_df$Title, "Throne of Glass")] <- "Throne of Glass"
+series1_df$Title[str_starts(series1_df$Title, "The Assassin")] <- "The Assassin"
+
+series1_pt2 <- series1_df %>% 
+  mutate(Title = na_if(series1_df$Title, "The Assassin")) %>% 
+  drop_na(Title)
+  
+checkouts_per_month <- series1_pt2 %>% 
+  group_by(Title, date) %>% 
+  summarize(total_checkouts = sum(Checkouts, na.rm = T))
+
+ggplot(data = checkouts_per_month) +
+  geom_line(mapping = aes(x = date, 
+                y = total_checkouts,
+                group = Title,
                 color = Title))
+
