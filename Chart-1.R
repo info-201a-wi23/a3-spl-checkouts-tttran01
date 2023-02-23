2e08cdb6f6ccf0a7c93b275c9dcb3a0a00bcf8ed
tttran01 <tran10@uw.edu>
2023-02-23 00:00:12 -0800

    add df for chart 1 and 2

--- Chart-1.R
+++ Chart-1.R
@@ -1,8 +1,10 @@
-library(dplyr)
 
-trend1_df <- spl_df %>% 
+series1_df <- spl_df %>% 
+  drop_na(Checkouts) %>% 
+  group_by(Title) %>% 
   filter(str_detect(Title, "Throne of Glass"))
-  
 
-ggplot(data = trend1_df) +
-  geom_line(aes(x=x_values, y = y_values))
+ggplot(data = series1_df) +
+  geom_line(aes(x = x_values, 
+                y = y_values,
+                color = Title))
--- Chart-2.R
+++ Chart-2.R
@@ -1,10 +1,14 @@
-library(dplyr)
+
+series2_df <- spl_df %>% 
+  drop_na(Checkouts) %>% 
+  group_by(Title) %>% 
+  filter(str_detect(Title, "Harry Potter")) 
+
+# For faster filtering
+series2_df <- series2_df %>% 
+  filter(str_detect(Creator, "Rowling"))
 
 
-trend2_df <- spl_df %>% 
-  
-
-y_values <- seq(1,3)
-
-ggplot(data = trend2_df) +
-  geom_line(aes(x=x_values, y = y_values))
+ggplot(data = series2_df) +
+  geom_line(aes(x=x_values,
+                y = y_values))
