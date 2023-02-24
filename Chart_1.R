0fdc4f298f5d87db4e91845f36b161cf393e4fda
tttran01 <tran10@uw.edu>
2023-02-23 19:25:32 -0800

    fix chart 1

--- Chart_1.R
+++ Chart_1.R
@@ -1,7 +1,7 @@
 
 series1_df <- spl_df %>% 
-  select(CheckoutYear, Checkouts, Title, Creator) %>% 
-  filter(str_detect(Title, "Throne of Glass")) 
+  select(date, Checkouts, Title) %>% 
+  filter(str_detect(Title, "Throne of Glass"))
 
 series1_df$Title[str_detect(series1_df$Title, "(?i)Crown")] <- "Crown of Midnight"
 series1_df$Title[str_detect(series1_df$Title, "(?i)Heir")] <- "Heir of Fire"
@@ -16,22 +16,23 @@ tog_df <- series1_df %>%
   mutate(Title = na_if(series1_df$Title, "The Assassin")) %>% 
   drop_na(Title)
   
-checkouts_per_year <- tog_df %>% 
-  group_by(Title, CheckoutYear) %>% 
+checkouts_per_month <- tog_df %>% 
+  group_by(Title, date) %>% 
   summarize(total_checkouts = sum(Checkouts, na.rm = T))
 
-# Make x-axis easier to see
-tog_chart <- ggplot(data = checkouts_per_year) +
-  geom_line(mapping = aes(x = CheckoutYear, 
-                y = total_checkouts,
-                group = Title,
-                color = Title,
-                text = paste0("Title:", Title, ", ", total_checkouts))) +
+tog_chart <- ggplot(data = checkouts_per_month) +
+  geom_line(mapping = aes(x = date, 
+                          y = total_checkouts,
+                          group = Title,
+                          color = Title,
+                          text = paste0("Title:", Title, ", ", total_checkouts))) +
   labs(title = "Throne of Glass Series Checkouts",
        x = "Dates",
        y = "Checkouts",
        color = "Books") +
-  scale_x_continuous(breaks = seq(2012, 2025, 1))
+  scale_x_date(NULL,
+               breaks = scales::breaks_width("2 years"),
+               labels = scales::label_date("'%y"))
 
 ggplotly(tog_chart, tooltip = "text")
 
