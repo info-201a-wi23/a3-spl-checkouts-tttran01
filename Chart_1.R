a24f88671bc477daaa95ae2bc844c3c6ee3b2da4
tttran01 <tran10@uw.edu>
2023-02-23 18:09:07 -0800

    fix chart 1 x axis

--- Chart_1.R
+++ Chart_1.R
@@ -1,27 +1,28 @@
 
 series1_df <- spl_df %>% 
-  select(CheckoutYear, CheckoutMonth, Checkouts, Title, Creator, date) %>% 
+  select(CheckoutYear, Checkouts, Title, Creator) %>% 
   filter(str_detect(Title, "Throne of Glass")) 
 
-series1_df$Title[str_detect(series1_df$Title, "Crown of Midnight")] <- "Crown of Midnight"
-series1_df$Title[str_detect(series1_df$Title, "Heir of Fire")] <- "Heir of Fire"
-series1_df$Title[str_detect(series1_df$Title, "Queen of Shadows")] <- "Queen of Shadows"
-series1_df$Title[str_detect(series1_df$Title, "Empire of Storms")] <- "Empire of Storms"
-series1_df$Title[str_detect(series1_df$Title, "Tower of Dawn")] <- "Tower of Dawn"
-series1_df$Title[str_detect(series1_df$Title, "Kingdom of Ash")] <- "Kingdom of Ash"
-series1_df$Title[str_starts(series1_df$Title, "Throne of Glass")] <- "Throne of Glass"
-series1_df$Title[str_starts(series1_df$Title, "The Assassin")] <- "The Assassin"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Crown")] <- "Crown of Midnight"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Heir")] <- "Heir of Fire"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Queen")] <- "Queen of Shadows"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Empire")] <- "Empire of Storms"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Dawn")] <- "Tower of Dawn"
+series1_df$Title[str_detect(series1_df$Title, "(?i)Kingdom")] <- "Kingdom of Ash"
+series1_df$Title[str_starts(series1_df$Title, "(?i)Throne of Glass")] <- "Throne of Glass"
+series1_df$Title[str_starts(series1_df$Title, "(?i)The Assassin")] <- "The Assassin"
 
-series1_pt2 <- series1_df %>% 
+tog_df <- series1_df %>% 
   mutate(Title = na_if(series1_df$Title, "The Assassin")) %>% 
   drop_na(Title)
   
-checkouts_per_month <- series1_pt2 %>% 
-  group_by(Title, date) %>% 
+checkouts_per_year <- tog_df %>% 
+  group_by(Title, CheckoutYear) %>% 
   summarize(total_checkouts = sum(Checkouts, na.rm = T))
 
-tog_chart <- ggplot(data = checkouts_per_month) +
-  geom_line(mapping = aes(x = date, 
+# Make x-axis easier to see
+tog_chart <- ggplot(data = checkouts_per_year) +
+  geom_line(mapping = aes(x = CheckoutYear, 
                 y = total_checkouts,
                 group = Title,
                 color = Title,
@@ -29,7 +30,8 @@ tog_chart <- ggplot(data = checkouts_per_month) +
   labs(title = "Throne of Glass Series Checkouts",
        x = "Dates",
        y = "Checkouts",
-       color = "Books")
+       color = "Books") +
+  scale_x_continuous(breaks = seq(2012, 2025, 1))
 
 ggplotly(tog_chart, tooltip = "text")
 
--- Chart_2.R
+++ Chart_2.R
@@ -1,12 +1,20 @@
 
 series2_df <- spl_df %>% 
-  drop_na(Checkouts) %>% 
-  group_by(Title) %>% 
-  filter(str_detect(Title, "Harry Potter")) 
-
-# For faster filtering
-series2_df <- series2_df %>% 
+  filter(str_detect(Title, "Harry Potter")) %>% 
   filter(str_detect(Creator, "Rowling"))
+
+series2_df$Title[str_detect(series2_df$Title, "(?i)Stone")] <- "Sorcerer's Stone"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Secrets")] <- "Chamber of Secrets"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Azkaban")] <- "Prisoner of Azkaban"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Goblet")] <- "Goblet of Fire"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Phoenix")] <- "Order of the Phoenix"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Prince")] <- "Half-Blood Prince"
+series2_df$Title[str_detect(series2_df$Title, "(?i)Hallows")] <- "Deathly Hallows"
+
+
+
+# For faster rendering
+
 
 
 ggplot(data = series2_df) +
